de3b7d1ea3bdcdecb5ac1fb481d4e9af304d5180
sanketnighot <sanketnighot25@gmail.com>
2022-10-20 10:16:33 +0530

    Init

--- Controller/jobController.js
+++ Controller/jobController.js
@@ -1,107 +1,43 @@
 const _ = require('lodash') 
 const axios = require('axios');
 require('dotenv').config();
-const { TezosToolkit } = require("@taquito/taquito");
-const { InMemorySigner } = require("@taquito/signer");
+const {Map} = require('../Models/mapModel');
 
-const rpcURL = process.env.RPCURL;
-const Tezos = new TezosToolkit(rpcURL);
+const checkBooking = async () => {
+    const tileCopy = await Map.find({status: "BOOKED"});
+    if (tileCopy) {
 
-Tezos.setProvider({
-    signer: new InMemorySigner(process.env.PVTKEY),
-});
-
-
-const distributeFunding = async () => {
-    let upcoming_funding_time;
-    let current_time = Date.parse(new Date().toUTCString())
-    await axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1WbA2H87o2RT9sTT4UaEgUAUgq6ZQhynbP/storage")
-        .then( async (storage) => {
-        upcoming_funding_time = Date.parse(new Date(storage.data.upcoming_funding_time).toUTCString())
-        funding_period = parseInt(storage.data.funding_period)
-        console.log(upcoming_funding_time, current_time)
-        const diff = upcoming_funding_time - current_time
-        console.log("------------>  Diff: ", diff)
-        if ((diff) <= 25000 ) {
-            await Tezos.contract
-            .at(process.env.VMMCONTRACT)
-            .then((contract) => {
-                contract.methods.distributeFunding().send().then(()=>{
-                console.log("Zenith :~ 🚀", "Txn Sent")
-            })
-            .catch((error) => {
-                console.log("Zenith :~ 🚀", "Error");
-                console.log(error)
-                distributeFunding();
-            });
-            })
-        } else {
-            console.log("Zenith :~ 🚀", "Not the funding time")
-        }
-    }).catch((error)=>{
-        console.log("err")
-        
-    });
-}
-
-const liquidatePosition = async () => {
-    const storage = await axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1WbA2H87o2RT9sTT4UaEgUAUgq6ZQhynbP/storage")
-    const positions = storage.data.positions
-    for (let address in positions) {
-        if (parseInt(positions[address].position) === 1) {
-            x = Math.abs(storage.data.vmm.vUSD_amount - (storage.data.vmm.invariant*1000000/(storage.data.vmm.token_amount + positions[address].position_value)))
-            final_value = positions[address].collateral_amount - x
-            if (final_value > 0) {
-            margin_ratio = Math.abs(final_value) * 1000000/positions[address].vUSD_amount
-                if (margin_ratio < 85000) {
-                    await Tezos.contract
-                        .at(process.env.VMMCONTRACT) 
-                        .then((contract) => {
-                            contract.methods.liquidate(address).send();
-                        }).then(()=>{
-                            console.log("Zenith :~ 🚀", "Liqudted")
-                        })
-                        .catch((error) => {
-                            console.log("Zenith :~ 🚀", "Error");
-                            console.log(error)
-                        });
-                } else {
-                    console.log("Zenith :~ 🚀", "No position to liquidate")
-                }
-            }
-
-        } else if (parseInt(positions[address].position) === 2){
-            x = storage.data.vmm.invariant*1000000/Math.abs(storage.data.vmm.token_amount - positions[address].position_value) - storage.data.vmm.vUSD_amount
-            final_value = positions[address].collateral_amount - Math.abs(x)
-            if (final_value > 0){
-                margin_ratio = Math.abs(final_value) * 1000000/positions[address].vUSD_amount
-                if (margin_ratio < 85000) {
-                    await Tezos.contract
-                        .at(process.env.VMMCONTRACT) 
-                        .then((contract) => {
-                            contract.methods.liquidate(address).send();
-                        }).then(()=>{
-                            console.log("Zenith :~ 🚀", "Liquidated")
-                        })
-                        .catch((error) => {
-                            console.log("Zenith :~ 🚀", "Error");
-                            console.log(error)
-                        });
-                } 
-            }
-        }
-      }
-    
-    
+        return res.status(200).send(tileCopy);
+    } else {
+        return res.status(404).send({message: 'No Booking Now'})
+    }
 }
 
 module.exports.executeJob = async (req, res) => {
-    await distributeFunding().then(()=> {
-        console.log("Zenith :~ 🚀", "Job executed 1 of 2")
-    });
-    await liquidatePosition().then(()=> {
-        console.log("Zenith :~ 🚀", "Job executed 2 of 2")
-    });
-    console.log(new Date().toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}));
-    // res.send("Done")
+    const tileCopy = await Map.find({status: "BOOKED"});
+    if (tileCopy.length != 0) {
+        for (land in tileCopy) {
+            const landData = await Map.findOne({x: tileCopy[land].x, y: tileCopy[land].y});
+            
+            let current_time = Date.parse(new Date().toUTCString())
+            let land_time = Date.parse(new Date(landData.updatedAt).toUTCString())
+            if ((current_time - land_time) > 60000) {
+                console.log((current_time - land_time), landData)
+                const landUpdate = await Map.findOneAndUpdate({x: tileCopy[land].x, y: tileCopy[land].y}, {status: ""}).then((newData)=>{
+                    if (newData.status === "") {
+                        console.log("Updated")
+                    }
+                })
+            }
+        }
+    }
+}
+
+module.exports.getBookedLands = async (req, res) => {
+    const tileCopy = await Map.find({status: "BOOKED"});
+    if (tileCopy.length != 0) {
+        return res.status(200).send(tileCopy);
+    } else {
+        return res.status(404).send({message: 'No Booking Now'})
+    }
 }
--- server.js
+++ server.js
@@ -2,14 +2,23 @@ require('dotenv/config');
 const app = require('./app');
 const {executeJob} = require('./Controller/jobController');
 var cron = require('node-cron');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
+require('dotenv/config');
+mongoose.connect(process.env.MONGODB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(()=> {
+        console.log('Connected to LOL MongoDb Server ...')
+    }).catch((err) =>{
+        console.log(`MongoDb Connection Failed: ${err}`);
+    });
 
-cron.schedule('*/60 * * * * *', () => {
+app.listen(PORT, () => {
+    console.log(`Server Running at http://localhost:${PORT} ...`);
+});
+
+cron.schedule('*/30 * * * * *', () => {
     executeJob();
     console.log("Job Executed", "⌛️" ,new Date().toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}));
   });
-
-
-app.listen(PORT, () => {
-    console.log(`Zenith :~ 🚀 Server Running`);
-});
